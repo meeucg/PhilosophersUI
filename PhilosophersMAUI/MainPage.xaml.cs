d8790fb0bf8e558f1b97db476d2292d3dc589ee4
meeucg <meeu.cg@gmail.com>
2024-05-07 22:03:09 +0300

    UI Update 1.0.0

--- PhilosophersMAUI/MainPage.xaml.cs
+++ PhilosophersMAUI/MainPage.xaml.cs
@@ -43,7 +43,7 @@ namespace PhilosophersMAUI
             {
                 if (sender is Button n)
                 {
-                    colors[buttonId] = (colors[buttonId] + 1) % CurrentTheme.Length;
+                    colors[buttonId] = (colors[buttonId] + 1) % CurrentTheme.Length;    
                     n.BackgroundColor = CurrentTheme[colors[buttonId]];
                     n.TextColor = CurrentTheme[colors[buttonId]];
                 }
@@ -62,8 +62,6 @@ namespace PhilosophersMAUI
                 WidthRequest = 150
             };
 
-            var mainLayout = new Grid();
-
             var functionalStack = new VerticalStackLayout
             {
                 Spacing = 50,
@@ -74,12 +72,19 @@ namespace PhilosophersMAUI
             functionalStack.Add(tableLayout);
             functionalStack.Add(iterateButon);
 
-            mainLayout.Add( functionalStack, 0, 0 );
-            mainLayout.Add(new BoxView 
+            mainLayout.Add(functionalStack, 0, 0);
+            Layout descriptionCanvas = new VerticalStackLayout
+            {
+                BackgroundColor = Colors.Black
+            };
+            descriptionCanvas.Add(new Label 
             { 
-                BackgroundColor = Colors.Silver
-            },
-            1, 0);
+                Text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+                TextColor = Colors.White, 
+                FontSize = 18,
+                Padding = 50
+            });
+            mainLayout.Add(descriptionCanvas, 1, 0);
 
             var scroll = new ScrollView
             {
@@ -87,6 +92,8 @@ namespace PhilosophersMAUI
             };
 
             Content = scroll;
+
+
         }
     }
 }
